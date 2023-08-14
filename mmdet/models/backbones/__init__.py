b81c4b943c1ef5e492868bfe9e2aa75b3a5cfee2
moqifeiliuming <3218872068@qq.com>
2023-08-14 16:12:13 +0800

    GF2

---
+++ mmdet/models/backbones/__init__.py
@@ -0,0 +1,19 @@
+from .darknet import Darknet
+from .detectors_resnet import DetectoRS_ResNet
+from .detectors_resnext import DetectoRS_ResNeXt
+from .hourglass import HourglassNet
+from .hrnet import HRNet
+from .regnet import RegNet
+from .res2net import Res2Net
+from .resnet import ResNet, ResNetV1d
+from .resnext import ResNeXt
+from .ssd_vgg import SSDVGG
+
+# 后续加上的，训练MAE模型
+from .madnas import MadNas
+
+__all__ = [
+    'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net',
+    'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt', 'Darknet',
+    'MadNas' # 后续加上的
+]
---
+++ tools/dist_train.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CONFIG=$1
+GPUS=$2
+PORT=${PORT:-29500}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+
+# python -m torch.distributed.launch --nproc_per_node=4 ./tools/train.py ./configs/gfocal_maedet/maedet_l_6x_lr0.02.py  --launcher pytorch
